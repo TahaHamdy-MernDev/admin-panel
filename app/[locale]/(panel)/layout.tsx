418b05507f05660678afc29d859ef71ca0423f88
TahaHamdy-MernDev <TahaHamdy.mern@gmail.com>
2026-02-01 03:34:55 +0200

    fix get cookies

--- app/[locale]/(panel)/layout.tsx
+++ app/[locale]/(panel)/layout.tsx
@@ -1,8 +1,9 @@
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import AppHeader from "@/components/sidebar/app-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { cookies } from "next/headers";
 import { redirect } from "@/i18n/navigation";
+
 export default async function Layout({
   children,
   params,
@@ -10,25 +11,24 @@ export default async function Layout({
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
 }) {
-  const access_token = (await cookies()).get("sa_access_token")?.value;
-  const refresh_token = (await cookies()).get("sa_refresh_token")?.value;
-  console.log({
-    refresh_token,
-    access_token,
-  });
+  const { locale } = await params;
 
-  if (!refresh_token || !access_token) {
-    const { locale } = await params;
-    return redirect({ href: "/login", locale });
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("sa_access_token")?.value;
+  const refreshToken = cookieStore.get("sa_refresh_token")?.value;
+
+  if (!accessToken || !refreshToken) {
+    redirect({ href: "/login", locale });
   }
+
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
         <AppHeader />
-        <div className="container mx-auto px-4 py-6 transition-all duration-200 ease-in">
+        <main className="container mx-auto px-4 py-6 transition-all duration-200 ease-in">
           {children}
-        </div>
+        </main>
       </SidebarInset>
     </SidebarProvider>
   );
