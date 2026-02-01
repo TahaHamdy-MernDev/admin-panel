e651cc1d0f9730c1529e04d312a63413e347d811
TahaHamdy-MernDev <TahaHamdy.mern@gmail.com>
2026-02-01 03:31:24 +0200

    update login form

--- features/auth/components/login-form.tsx
+++ features/auth/components/login-form.tsx
@@ -44,7 +44,7 @@ export function LoginForm() {
         case PlatformRoles.ACCOUNTANT:
         case PlatformRoles.ADMIN:
         case PlatformRoles.CALL_SERVICE:
-          router.replace("/overview");
+          router.push({ pathname: "overview" });
           break;
 
         default:
