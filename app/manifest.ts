d13fa5bc1eb1ef3fbddd22ac7d88f57b8274a552
TahaHamdy-MernDev <TahaHamdy.mern@gmail.com>
2026-02-09 19:46:16 +0200

    edit manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,17 +1,8 @@
 import { MetadataRoute } from "next";
-import { getTranslations } from "next-intl/server";
 
 export default async function manifest(): Promise<MetadataRoute.Manifest> {
-  // Pick a locale that is representative of the app
-  const locale = "en";
-
-  const t = await getTranslations({
-    namespace: "Manifest",
-    locale,
-  });
-
   return {
-    name: t("name"),
+    name: "Egapy Admin Panel",
     start_url: "/",
     theme_color: "#101E33",
     display: "standalone",
